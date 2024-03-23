3d56c8e637d8ec733bc757fa8f425353d0b52f45
AdiCotirla <adioprea53@gmail.com>
2024-03-23 19:31:17 +0200

    sda

--- src/Data/displayData.js
+++ src/Data/displayData.js
@@ -5,22 +5,22 @@ export const displayData = [
     {
         img: salon,
         name : "Salon",
-        atu : "/BS/Gallery/Salon"
+        atu : "BS/Gallery/Salon"
     },
     {
         img: salon,
         name : "Male",
-        atu : "/BS/Gallery/Male"
+        atu : "BS/Gallery/Male"
     },
     {
         img: salon,
         name : "Female",
-        atu : "/BS/Gallery/Female"
+        atu : "BS/Gallery/Female"
     },
     {
         img: salon,
         name : "Video",
-        atu : "/BS/Gallery/Video"
+        atu : "BS/Gallery/Video"
     },
 
 ]
